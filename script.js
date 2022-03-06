54d0a48cc5c8a684ae721cd41589e92bb9f81298
IfatNeumann <ineumann19@gmail.com>
2022-03-06 21:30:47 +0000

    basic js file + attached functions to items, created escape button + escaped page

---
+++ script.js
@@ -0,0 +1,19 @@
+function escape(){
+    location.href = "anotherPage.html";
+}
+
+function tryDoor(){
+    alert("Sorry, it's locked!");
+}
+
+function tryLock(){
+    alert("This is the lock");
+}
+
+function tryKnob(){
+    alert("This is the door knob");
+}
+
+function pickUpKey(){
+    alert("This is the key");
+}
